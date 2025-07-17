14c19250f446882418a01a36dbe179f68e63e611
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-17 10:56:41 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-MODE: 'public', // public or private
+MODE: 'private', // public or private
 SESSION_ID: process.env.SESSION_ID || 'LORD~OBITO~zU0UgYYJ#KdmqwzOFpfH73Ba2P0oGWBBGsDg-i7v6KR9zT2oxtC4',
 OWNER_NUMBER: '2250712668494',
 OWNER_NAME: '✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞',
 CHANNEL_URL: 'https://whatsapp.com/channel/0029Vb65HSyHwXbEQbQjQV26',
-AUTO_REACT: true,
+AUTO_REACT: false,
 PREFIX: '.', // <-- here is your command prefix
 BOT_NAME: 'LORD_OBITO-XMD-V2', // Nom du bot utilisé dans les messages stylés
 WELCOME_ENABLED: false, // Active/désactive la commande welcome
