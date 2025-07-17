2672c39dd2f309e3146ad76712b8c4b8d43819ef
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-17 10:03:10 +0000

    Add files via upload

--- data/callupdate.js
+++ data/callupdate.js
@@ -1,1 +1,19 @@
+import chalk from 'chalk';
 
+export async function Callupdate(call, sock) {
+  try {
+    for (const update of call) {
+      const callerId = update.from;
+      console.log(chalk.red(`📞 Call detected from ${callerId}`));
+
+      if (update.status === 'offer') {
+        await sock.updateBlockStatus(callerId, 'block');
+        await sock.sendMessage(callerId, {
+          text: `🚫 Calls are barred. You have been temporarily blocked..`,
+        });
+      }
+    }
+  } catch (err) {
+    console.error("❌ Erreur Callupdate:", err);
+  }
+}
